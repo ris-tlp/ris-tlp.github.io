874aacaffa2ba1958011e0a64664de79fa007ad7
opkhan27 <opkhan27@umd.edu>
2025-05-21 02:41:48 -0400

    Homepage mvp

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import './App.css'
+import { FaLinkedin, FaInstagram, FaGithub, FaEnvelope } from "react-icons/fa";
 
 function App() {
   return (
     <>
-      {/* <div className="bg-sage-green">
-        <p className="text-6xl font-raleway">Hello</p>
-      </div> */}
-      <section className="flex justify-between items-center h-screen">
+      <section className="flex flex-col md:flex-row justify-center md:justify-between md:items-center h-screen">
         <div className="flex-col">
           <h1 className="text-8xl font-bold">omar khan.</h1>
-          <div id="icons" className="flex">
-            <p>linkedin.</p>
-            <p>instagram.</p>
-            <p>github.</p>
+          <div id="icons" className="flex space-x-4 mt-4 justify-center">
+            <a href="https://www.linkedin.com/in/omar-pk/" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin className="text-3xl hover:text-[#f6f2b8] transition" />
+            </a>
+            <a href="https://github.com/ris-tlp" target="_blank" rel="noopener noreferrer">
+              <FaGithub className="text-3xl hover:text-white transition" />
+            </a>
+            <a href="mailto:omarkhantlp@gmail.com" target="_blank" rel="noopener noreferrer">
+              <FaEnvelope className="text-3xl hover:text-white transition" />
+            </a>
           </div>
         </div>
 
-        <p className="text-lg max-w-1/3">software engineer, audiophile, avid reader, sandwich enthusiast</p>
+        <p className="text-xl mt-8 md:mt-0">software engineer • audiophile • amatuer astronomer • sandwich enthusiast</p>
       </section>
     </>
   )
