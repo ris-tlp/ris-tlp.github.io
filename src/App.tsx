3b33a05a624568bc7c96aad7c8e7e2c2c6044b9e
opkhan27 <opkhan27@umd.edu>
2025-05-21 02:20:20 -0400

    Sample text

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,21 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className="bg-sage-green">
+      {/* <div className="bg-sage-green">
         <p className="text-6xl font-raleway">Hello</p>
-      </div>
+      </div> */}
+      <section className="flex justify-between items-center h-screen">
+        <div className="flex-col">
+          <h1 className="text-8xl font-bold">omar khan.</h1>
+          <div id="icons" className="flex">
+            <p>linkedin.</p>
+            <p>instagram.</p>
+            <p>github.</p>
+          </div>
+        </div>
+
+        <p className="text-lg max-w-1/3">software engineer, audiophile, avid reader, sandwich enthusiast</p>
+      </section>
     </>
   )
 }
