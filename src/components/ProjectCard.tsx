7fe0b4cb7411b263ab6700e8f93766eb7ca24686
opkhan27 <opkhan27@umd.edu>
2025-05-22 12:39:56 -0400

    Responsiveness for projects section

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -24,11 +24,11 @@ export default function ProjectCard({
     return (
         <div className="flex flex-col md:flex-row items-center justify-between w-full mb-6 space-y-6 md:space-y-0">
             <div className={`w-full md:w-1/3 ${reverse ? "md:order-2" : ""}`}>
-                <img src={imageUrl} className="h-48 w-96 rounded-lg object-cover" />
+                <img src={imageUrl} className="h-48 w-96 rounded-lg object-cover mx-auto md:mx-0" />
             </div>
 
             <div className={`flex flex-col md:w-2/3 mx-4 text-cream ${reverse ? "md:items-end" : "md:items-start"}`}>
-                <div className="flex items-center mb-4">
+                <div className="flex items-center justify-center text-center mb-4">
                     <h2 className="text-2xl font-bold">{title}</h2>
                     {/* Conditionally render either GitHub or Website Icon */}
                     {isGithub ? (
@@ -54,7 +54,7 @@ export default function ProjectCard({
 
                 <p className={`mb-4 ${reverse ? "md:text-right" : "md:text-left"}`}>{description}</p>
 
-                <div className="flex flex-wrap gap-2 mb-4">
+                <div className="flex flex-wrap justify-center gap-2 mb-4">
                     {techStack.map((tech, index) => (
                         <span key={index} className="text-xs text-cream px-2 py-1 rounded-full">
                             {tech}
