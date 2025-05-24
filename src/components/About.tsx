acf434bcb743830cdadf128c942b2630354326f7
opkhan27 <opkhan27@umd.edu>
2025-05-24 13:45:53 -0400

    Add another paragraph to about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,8 @@
 export default function About() {
   let aboutMeItems: string[] = [
     "I'm a software engineer based in Virginia with over a year of experience across the entire stack. I take pride in my craft and thrive in environments where I’m tasked with solving problems under tight constraints and ambiguous requirements. I genuinely enjoy the challenge of finding solutions within these limitations while balancing user satisfaction and business objectives.",
-    "I try and prioritize simplicity in my solutions, understanding that while complexity comes easily, simplicity requires efford to identify, design, and implement. Balancing simplicity with business goals is often the greater challenge, and it's a puzzle I genuinely enjoy solving.",
-    // "Software engineering has always been a core part of my routine, whether it’s learning from the experiences of seasoned engineers and large enterprises, or working on personal projects with a hobby or two mixed in. My goal is to build solid, practical knowledge so that I can eventually give back by sharing what I’ve learned through technical articles to help other engineers grow.",
+    "I try and prioritize simplicity in my solutions, understanding that while complexity comes easily, simplicity requires effort to identify, design, and implement. Balancing simplicity with business goals is often the greater challenge, and it's a puzzle I truly enjoy solving.",
+    "Learning software engineering has always been a core part of my routine, whether it’s through reading the experiences of seasoned engineers and large enterprises, or working on personal projects with a hobby or two mixed in. My goal is to build solid, practical knowledge so that I can eventually give back by sharing what I’ve learned through technical articles to help other engineers grow.",
     "Outside of work, I enjoy observing the Messier catalog through my Dobsonian, jamming out to garage rock and countless other genres with my open-backs and IEMs, or spending time in the kitchen finding the perfect symphony of sauces for my steak and cheese sandwich."
   ];
 
